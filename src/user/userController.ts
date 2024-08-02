2950295598376c39b0d6e72cb95a11494ec42705
subx6789 <subhajits956@gmail.com>
2024-08-02 12:56:26 +0530

    added register functionality

--- src/user/userController.ts
+++ src/user/userController.ts
@@ -1,8 +1,37 @@
 import { NextFunction, Request, Response } from "express";
-
+import createHttpError from "http-errors";
+import userModel from "./userModel";
+import bcrypt from "bcrypt";
+import { sign } from "jsonwebtoken";
+import { config } from "../config/config";
 const createUser = async (req: Request, res: Response, next: NextFunction) => {
+  const { name, email, password } = req.body;
+  //Validation
+  if (!name || !email || !password) {
+    const error = createHttpError(400, "All fields are required");
+    return next(error);
+  }
+
+  //Database call
+  const user = await userModel.findOne({ email });
+  if (user) {
+    const error = createHttpError(400, "User already exists with this email");
+    return next(error);
+  }
+  //Process
+  const salt = await bcrypt.genSalt(10);
+  const hashedPassword = await bcrypt.hash(password, salt);
+  const newUser = await userModel.create({
+    name,
+    email,
+    password: hashedPassword,
+  });
+  //Response
+  const token = sign({ sub: newUser._id }, config.jwtSecret as string, {
+    expiresIn: "7d",
+  });
   res.json({
-    message: "User registered",
+    accessToken: token,
   });
 };
 export { createUser };
